2a14331d4c6c91c2fa7a5a7d6d78343b3130c926
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-16 11:25:37 +0200

    Add and style background image

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import ThemeControl from './components/ThemeControl';
 import AddTask from './components/AddTask';
 import TaskList from './components/TaskList';
 
+import backgroundImage from './images/bg-desktop-dark.jpg';
+
 function App() {
   return (
     <>
+    <div className="background-image">
+      <img src={backgroundImage} alt="Background" />
+    </div>
     <ThemeControl/>
     <AddTask/>
     <TaskList/>
