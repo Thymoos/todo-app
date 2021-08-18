5129a71817750920c4f194dcb18dd5cc9d0f0e1b
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-18 13:05:33 +0200

    Add delete task button

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './TaskList.css';
 
+import deleteBtn from '../images/icon-cross.svg';
+
+import FilterControl from './FilterControl';
+
 const TaskList = () => {
     return ( 
+        <div className="shadow-main">
         <div className="task-list">
-            Task list
+            <div className="task"><input type="checkbox" name="value" id="1" /><label htmlFor="1"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="2" /><label htmlFor="2"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="3" /><label htmlFor="3"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="4" /><label htmlFor="4"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="5" /><label htmlFor="5"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="6" /><label htmlFor="6"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="7" /><label htmlFor="7"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            <div className="task"><input type="checkbox" name="value" id="8" /><label htmlFor="8"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+
+        </div>
+        <FilterControl/>
         </div>
      );
 }
- 
+
+// Labels are needed for checkboxes to work
+
 export default TaskList;
