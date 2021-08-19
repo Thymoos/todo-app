02934baf35c4b14062f5f8cb5e8905afc7dee29c
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-19 10:47:11 +0200

    Use .map() to display tasks from state

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './TaskList.css';
 
 import deleteBtn from '../images/icon-cross.svg';
 
 import FilterControl from './FilterControl';
 
+const defaultTaskList = [
+    {
+        id: 0,
+        text: "Do the laundry",
+        completed: true
+    },
+    {
+        id: 1,
+        text: "Finish todo-app",
+        completed: false
+    },
+    {
+        id: 2,
+        text: "Try to look good",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Woo some girls",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Order pizza",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Check mail",
+        completed: true
+    }
+];
+
 const TaskList = () => {
+
+    const [tasks, setTasks] = useState(defaultTaskList);
+
     return ( 
         <div className="shadow-main">
         <div className="task-list">
-            <div className="task"><input type="checkbox" name="value" id="1" /><label htmlFor="1"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="2" /><label htmlFor="2"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="3" /><label htmlFor="3"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="4" /><label htmlFor="4"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="5" /><label htmlFor="5"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="6" /><label htmlFor="6"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="7" /><label htmlFor="7"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
-            <div className="task"><input type="checkbox" name="value" id="8" /><label htmlFor="8"></label><p>Complete online JavaScript course</p><img src={deleteBtn} alt="X" /></div>
+            
+            {tasks.map((task, index) => (
+                <div className="task"><input type="checkbox" name="value" id={index} /><label htmlFor={index}></label><p>{task.text}</p><img src={deleteBtn} alt="X" /></div>
+            ))}
 
         </div>
         <FilterControl/>
