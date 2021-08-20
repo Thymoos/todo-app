5592093a2de4fa881e69d0cd62b7f9a41bd61d92
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-20 14:46:26 +0200

    Add filter buttons styling

--- src/components/FilterControl.js
+++ src/components/FilterControl.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './FilterControl.css';
 
 const FilterControl = ({activeTasks}) => {
+
     return ( 
         <div className="filter-wrapper">
             <p>{activeTasks} items left</p>
             <div className="filter-options">
-                <p>All</p>
-                <p>Active</p>
-                <p>Completed</p>
+
+                <input type="radio" name="filter-group" id="all" defaultChecked/><label htmlFor="all">All</label>
+                <input type="radio" name="filter-group" id="active"/><label htmlFor="active">Active</label>
+                <input type="radio" name="filter-group" id="completed"/><label htmlFor="completed">Completed</label>
+
             </div>
             <p>Clear completed</p>
         </div>
