a5413897082838fd8a35bf873c895b3f88b50242
Konrad Izdebski <konradizdebski1011@gmail.com>
2021-08-24 07:43:22 +0200

    Add theme change functionality

--- src/components/ThemeControl.js
+++ src/components/ThemeControl.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ThemeControl.css';
 
 import sunThemeIcon from '../images/icon-sun.svg';
+import moonThemeIcon from '../images/icon-moon.svg';
 
 const ThemeControl = () => {
+
+    const [theme, setTheme] = useState(false);
+
+    const handleThemeChange = () => {
+        const body = document.body;
+        
+        body.className = "";
+        
+        if (theme === false) {
+            body.className = "light-theme";
+            setTheme(true);
+        }
+        else {
+            body.className = "dark-theme";
+            setTheme(false);
+        }
+    }
+
     return ( 
         <div className="title-wrapper">
             <h1>TODO</h1>
-            <button><img src={sunThemeIcon} alt="Theme icon" /></button> 
+            <button>
+                <img src={theme ? moonThemeIcon : sunThemeIcon} alt="Theme icon" 
+                onClick={handleThemeChange}
+            /></button> 
         </div>
      );
 }
